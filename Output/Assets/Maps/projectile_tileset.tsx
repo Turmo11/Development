5087d67b50ce092e5d67dbdc60266ac854292e30
Turmo11 <Turmo11@users.noreply.github.com>
2020-12-06 00:24:49 +0100

    Updated new tilesets on second level

---
+++ Output/Assets/Maps/projectile_tileset.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="projectile_tileset" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../Textures/Player/projectile.png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="beam" value="beam"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="70"/>
+   <frame tileid="0" duration="70"/>
+   <frame tileid="0" duration="70"/>
+   <frame tileid="1" duration="70"/>
+   <frame tileid="1" duration="70"/>
+   <frame tileid="1" duration="70"/>
+   <frame tileid="2" duration="70"/>
+   <frame tileid="2" duration="70"/>
+   <frame tileid="2" duration="70"/>
+   <frame tileid="3" duration="70"/>
+   <frame tileid="3" duration="70"/>
+   <frame tileid="3" duration="70"/>
+  </animation>
+ </tile>
+</tileset>
